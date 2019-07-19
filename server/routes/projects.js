0e1469842d7136e41e8ab5b37a73ac47e704825a
mjarraya <mjarraya@student.42.fr>
2019-07-19 11:31:40 +0200

    get all projects

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -5,7 +5,14 @@ const Project = require("../models/Project");
 /* GET home page */
 router.get("/", (req, res, next) => {
   //   get all projects
-  //
+  // instead of res.render() -> res.json()
+  Project.find()
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
