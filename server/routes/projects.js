3467ef450ae1a4ded326b2e325f483b6f94a9a14
mjarraya <mjarraya@student.42.fr>
2019-07-19 11:15:09 +0200

    project routes and model setup

---
+++ server/routes/projects.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const Project = require("../models/Project");
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  //   get all projects
+  //
+});
+
+module.exports = router;
