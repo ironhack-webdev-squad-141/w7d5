1ea0d6be179d2890fa5a2ebe2da1f1d75d5ee96b
mjarraya <mjarraya@student.42.fr>
2019-07-19 12:17:39 +0200

    projects routes

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -3,12 +3,64 @@ const router = express.Router();
 const Project = require("../models/Project");
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", (req, res) => {
   //   get all projects
   // instead of res.render() -> res.json()
   Project.find()
     .then(projects => {
       res.json(projects);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Project.findById(id)
+    .then(project => {
+      res.json(project);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  // const title = req.body.title
+  // const description = req.body.description
+  const { title, description } = req.body;
+
+  // Project.create({ title: title, description: description })
+  Project.create({ title, description })
+    .then(project => {
+      res.json(project);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const { title, description } = req.body;
+
+  Project.findByIdAndUpdate(id, { title, description })
+    .then(() => {
+      res.json({ message: `Project with id ${id} was successfully updated` });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Project.findByIdAndDelete(id)
+    .then(() => {
+      res.json({ message: `Project with id ${id} was successfully deleted` });
     })
     .catch(err => {
       res.json(err);
